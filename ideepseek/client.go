ec75c4d553acec590dbd254d55c63fa4a915b4cf
lcsin <1847@qq.com>
2025-01-23 17:10:03 +0800

    new

--- ideepseek/client.go
+++ ideepseek/client.go
@@ -1,8 +1,8 @@
 package ideepseek
 
 const (
-	ModelV3 = "deepseek-chat"
-	ModelR1 = "deepseek-reasoner"
+	V3 = "deepseek-chat"
+	R1 = "deepseek-reasoner"
 
 	RoleSystem    = "system"
 	RoleUser      = "user"
@@ -10,18 +10,25 @@ const (
 )
 
 type Client struct {
-	Url    string
-	Apikey string
-	Model  string
-	Stream bool
+	Url    string // 接口地址：https://api.deepseek.com
+	Apikey string // apikey
+
+	Model  string // 调用模型
+	Stream bool   // 是否流式
+	// 代码生成/数学解题	0.0
+	// 数据抽取/分析	1.0
+	// 通用对话	1.3
+	// 翻译	1.3
+	// 创意类写作/诗歌创作	1.5
+	Temperature float64 // 温度，默认1.0
+	MaxTokens   int64   // 最大输出长度，默认为4k
 }
 
 func NewClient(url, apikey string) *Client {
 	return &Client{
 		Url:    url,
 		Apikey: apikey,
-		Model:  ModelV3,
-		Stream: false,
+		Model:  V3,
 	}
 }
 
@@ -32,5 +39,15 @@ func (c *Client) WithModel(model string) *Client {
 
 func (c *Client) WithStream(stream bool) *Client {
 	c.Stream = stream
+	return c
+}
+
+func (c *Client) WithTemperature(temperature float64) *Client {
+	c.Temperature = temperature
+	return c
+}
+
+func (c *Client) WithMaxTokens(maxTokens int64) *Client {
+	c.MaxTokens = maxTokens
 	return c
 }
